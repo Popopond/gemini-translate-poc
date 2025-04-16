9493ece1b8b3d6c91b745f0ccaf034d608658ac8
Pond <pimpakan2545@gmail.com>
2025-04-16 17:00:33 +0700

    Initial commit

---
+++ src/main/java/com/example/poc_gemini/PocGeminiApplication.java
@@ -0,0 +1,13 @@
+package com.example.poc_gemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocGeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocGeminiApplication.class, args);
+	}
+
+}
